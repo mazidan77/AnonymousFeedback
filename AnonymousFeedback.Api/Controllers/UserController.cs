bda91b42e8f6b730f4eef8d690687fc3b62f1636
mazidan77 <moga4arab@gmail.com>
2024-05-05 15:15:41 +0300

    Add User Controller

---
+++ AnonymousFeedback.Api/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using AnonymousFeedback.Application.Dtos.UserDto;
+using AnonymousFeedback.Domian.IManagers;
+using AnonymousFeedback.Domian.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Contracts;
+
+namespace AnonymousFeedback.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IBaseManager<User> _userManager;
+        private IMapper  _mapper;
+
+        public UserController(IBaseManager<User> userManager, IMapper mapper)
+        {
+            _mapper = mapper;
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public IActionResult GetAllUser()
+        {
+            var users = _userManager.GetAll();
+            var usersDto = _mapper.Map<List<UserGetDto>>(users);
+            return Ok(usersDto);
+        }
+
+        [HttpPut]
+        public IActionResult EditUser(UserPutDto userDto) {
+
+            var entity = _mapper.Map<User>(userDto);
+            var user = _userManager.UpdateWithComplete(entity);
+            return Ok(userDto);
+
+        
+        }
+    }
+}
